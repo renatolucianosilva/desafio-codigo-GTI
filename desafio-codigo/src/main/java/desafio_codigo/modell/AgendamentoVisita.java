7d0172b9efd55a0b54c9884687c4b450ffb43aba
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-20 21:58:33 -0300

    feat: alterarDataHoraVisita

--- desafio-codigo/src/main/java/desafio_codigo/modell/AgendamentoVisita.java
+++ desafio-codigo/src/main/java/desafio_codigo/modell/AgendamentoVisita.java
@@ -3,7 +3,10 @@ package desafio_codigo.modell;
 import desafio_codigo.api.request.AgendamentoVisitaRequest;
 import desafio_codigo.exceptions.BadRequestException;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +14,9 @@ import java.time.LocalDateTime;
 @Data
 @Entity
 @Table(name="agendamento_visita")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AgendamentoVisita {
 
     @Id
@@ -33,32 +39,24 @@ public class AgendamentoVisita {
     @JoinColumn(name="status_id")
     private Status status;
 
-    public AgendamentoVisita novoAgendamentoVisita(Custodiado custodiado, Visitante visitante, Status status ) {
 
-        this.custodiado = custodiado;
-        this.visitante = visitante;
-        this.status = status;
 
-        return this;
-    }
 
     public AgendamentoVisita cancelarVisita(Status status) {
 
-        if(this.status.getDescricao().equals(status.getDescricao()))
+        if(this.status.getDescricao().equals("CANCELADO"))
             throw new BadRequestException("Agendamento com Status de Cancelado");
-
-
         this.status = status;
+
 
         return this;
     }
 
-    public AgendamentoVisita validarDataHoraAgendamento(LocalDateTime dataHoraAgendamento) {
-
-        var dataAgendamento = dataHoraAgendamento.toLocalDate();
-        var horaAgendamento = dataHoraAgendamento.toLocalTime();
-
-        return null;
+    public AgendamentoVisita alteraDataHoraVisita(LocalDateTime dataHoraAgendamento) {
+        this.dataHoraAgendamento = dataHoraAgendamento;
+        return this;
     }
+
+
 
 }
