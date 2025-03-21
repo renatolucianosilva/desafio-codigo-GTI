693e333f46f316ff0aee4a09f46345ec1de233a6
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-21 01:58:36 -0300

    feat: JwT e OAuth

---
+++ desafio-codigo/src/main/java/desafio_codigo/configurations/UserConfig.java
@@ -0,0 +1,39 @@
+package desafio_codigo.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<GrantedAuthority> authorities = Arrays.asList("SCOPE_read", "SCOPE_write")
+                .stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
