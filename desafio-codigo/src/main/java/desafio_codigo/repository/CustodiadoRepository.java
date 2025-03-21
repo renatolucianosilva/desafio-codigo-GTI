95605e8f3b78c84708e393ba9174a21ab378d580
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-20 21:53:31 -0300

    Configurações de consulta

--- desafio-codigo/src/main/java/desafio_codigo/repository/CustodiadoRepository.java
+++ desafio-codigo/src/main/java/desafio_codigo/repository/CustodiadoRepository.java
@@ -4,8 +4,14 @@ import desafio_codigo.modell.Custodiado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustodiadoRepository extends JpaRepository<Custodiado, Long> {
 
+    Optional<List<Custodiado>> findByNome(String nome);
+
+    Optional<Custodiado> findCustodiadoByNomeAndNumeroProntuario(String nome, String numeroProntuario);
 
 }
